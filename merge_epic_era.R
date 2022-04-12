53f6552dbf551c5cf4e3db59a847be3614124d28
Ryan <c.ryan.king@gmail.com>
2022-04-12 17:23:27 -0500

    added CPT data

--- merge_epic_era.R
+++ merge_epic_era.R
@@ -25,6 +25,7 @@ epic_admits <- fread(paste0(clarity_root , 'Clarity Hogue Result Set ADT.csv'))
 my_visits <- epic_admits[ADT_EVENT %chin% c("Admission","Discharge" , "Hospital Outpatient") , .(CurrentMRN = first(CurrentMRN), admt = min(EFFECTIVE_TIME), dist=max(EFFECTIVE_TIME) ) , by="CSN"]
 
 
+
 ## filter to just the variables of interest
 merged_data <- processed_preop_screen[, .(orlogid, AD8=`AD8 Dementia Score`, SBT=`Short Blessed Total Score` ) ] %>% 
 merge(admit_outcomes[ , .(orlogid,ICU,  postop_los, readmit=fcase(is.na(readmission_survival) , FALSE, readmission_survival>30, FALSE, default=TRUE ) ) ] , by="orlogid") %>%
@@ -43,28 +44,28 @@ epic_id2 <- epic_id2[!is.na(MAR_WINDOW_END)]
 
 procedure_codes <- epic_id2[ , .(orlogid=LOG_ID, CSN=AN_3_ENC_CSN_ID, AN_PROC_NAME, AN_DATE)][procedure_codes[, .(CSN=`Encounter Epic CSN` %>% as.character, codes=`Procedure Code Concat (1-50)`, dist=`Discharge Date`, admt=`Admit Date`, dispo=`Discharge Disposition`) ]  , on="CSN"]
 
-procedure_codes[, dispo:= fcase(dispo,
-  "RTN" , "home", ## home or self care
-  "ORH" , "facility", ## inpatient rehab
-  "HHC" , "home", ## with home health
-  "SNF", "facility",
-  "ICF", "facility", ## custodial facility = ward of state?
-  "HSM", "death", ## hospice
-  "SWG", "facility", ## swing bed = SNF/ inpt hybrid
-  "EXP", "death", 
-  "AMA", "home",
-  "COL", "home", ## jail
-  "OIN", "facility", ## transfer to cancer center
-  "LTC", "facility",
-  "HOS", "death", ## hospice
-  "LWB", "home", ## also AMA
-  "FED", "facility", ## VA
-  "PSY", "facility", 
-  "DRR", "facility", ## transfer, maybe make NA?
-  "LRP", NA_character_ ## diverted elsewhere - these are probably errors
-  "OTH", "facility", ## transfer / inpatient rehab
-  "DBR", NA_character_, ## I think an error code
-  "NSF", "facility", ## SNF also
+procedure_codes[, dispo:= fcase(
+  dispo=="RTN" , "home", ## home or self care
+  dispo=="ORH" , "facility", ## inpatient rehab
+  dispo=="HHC" , "home", ## with home health
+  dispo=="SNF", "facility",
+  dispo=="ICF", "facility", ## custodial facility = ward of state?
+  dispo=="HSM", "death", ## hospice
+  dispo=="SWG", "facility", ## swing bed = SNF/ inpt hybrid
+  dispo=="EXP", "death", 
+  dispo=="AMA", "home",
+  dispo=="COL", "home", ## jail
+  dispo=="OIN", "facility", ## transfer to cancer center
+  dispo=="LTC", "facility",
+  dispo=="HOS", "death", ## hospice
+  dispo=="LWB", "home", ## also AMA
+  dispo=="FED", "facility", ## VA
+  dispo=="PSY", "facility", 
+  dispo=="DRR", "facility", ## transfer, maybe make NA?
+  dispo=="LRP", NA_character_, ## diverted elsewhere - these are probably errors
+  dispo=="OTH", "facility", ## transfer / inpatient rehab
+  dispo=="DBR", NA_character_, ## I think an error code
+  dispo=="NSF", "facility", ## SNF also
   default=NA_character_) ]
 
 ## unfortunately, the codes are a comma separated column
@@ -90,6 +91,43 @@ gut_codes = c("0D[BTVD5][89ABEFGHKLMNPQ]"  )
 , ueavfist_codes = c("031[345678569ABCH]0[A-Z0-9][DF]","03WY0J","05WY0[JK]Z" , "36830" , "36818", "36818", "36819", "36821", "36833", "36832", "36825")
 , vats_codes = c("0BT[CDFGHJK]4ZZ")
 )
+
+
+## add in CPT codes from AMO
+cpt_code_pattern <- readxl::read_xlsx("/research/ActFast_BJ_Data/External_Dictionaries/CPT codes.xlsx")
+cpt_code_pattern <- cpt_code_pattern[is.na(cpt_code_pattern$drop),]
+
+code_patterns$gut_codes <- c(code_patterns$gut_codes, cpt_code_pattern[cpt_code_pattern$group==2 , "code", drop=TRUE] ) %>% unique
+
+code_patterns$stomach_codes <- c(code_patterns$stomach_codes, cpt_code_pattern[cpt_code_pattern$group==3 , "code", drop=TRUE] ) %>% unique
+
+code_patterns$chole_codes <- c(code_patterns$chole_codes, cpt_code_pattern[cpt_code_pattern$group==1 , "code", drop=TRUE] ) %>% unique
+
+code_patterns$panc_codes <- c(code_patterns$panc_codes, cpt_code_pattern[cpt_code_pattern$group==4 , "code", drop=TRUE] ) %>% unique
+
+code_patterns$hyster_codes <- c(code_patterns$hyster_codes, cpt_code_pattern[cpt_code_pattern$group==5 , "code", drop=TRUE] ) %>% unique
+
+code_patterns$lumbar_codes <- c(code_patterns$lumbar_codes, cpt_code_pattern[cpt_code_pattern$group==6 , "code", drop=TRUE] ) %>% unique
+
+code_patterns$shoulder_codes <- c(code_patterns$shoulder_codes, cpt_code_pattern[cpt_code_pattern$group==9 , "code", drop=TRUE] ) %>% unique
+
+code_patterns$hiatalHernia_codes <- c(code_patterns$hiatalHernia_codes, cpt_code_pattern[cpt_code_pattern$group==10 , "code", drop=TRUE] ) %>% unique
+
+code_patterns$knee_codes <- c(code_patterns$knee_codes, cpt_code_pattern[cpt_code_pattern$group==7 , "code", drop=TRUE] ) %>% unique
+
+code_patterns$totalHip_codes <- c(code_patterns$totalHip_codes, cpt_code_pattern[cpt_code_pattern$group==8 , "code", drop=TRUE] ) %>% unique
+
+code_patterns$neph_codes <- c(code_patterns$neph_codes, cpt_code_pattern[cpt_code_pattern$group==12 , "code", drop=TRUE] ) %>% unique
+
+code_patterns$prost_codes <- c(code_patterns$prost_codes, cpt_code_pattern[cpt_code_pattern$group==13 , "code", drop=TRUE] ) %>% unique
+
+code_patterns$bladder_codes <- c(code_patterns$bladder_codes, cpt_code_pattern[cpt_code_pattern$group==14 , "code", drop=TRUE] ) %>% unique
+
+code_patterns$ueavfist_codes <- c(code_patterns$ueavfist_codes, cpt_code_pattern[cpt_code_pattern$group==15 , "code", drop=TRUE] ) %>% unique
+
+code_patterns$vats_codes <- c(code_patterns$vats_codes, cpt_code_pattern[cpt_code_pattern$group==11 , "code", drop=TRUE] ) %>% unique
+
+
 
 ## testing
 if(FALSE) {
@@ -143,11 +181,66 @@ if(FALSE) {
 
 }
 
+
+
+if(TRUE) {
+## an aside -- figure 1 type flow
+## only 16 k hae an AD8+SBT, 30k one or the other
+processed_preop_screen[, .(orlogid, AD8=`AD8 Dementia Score`, SBT=`Short Blessed Total Score` ) ] [ !is.na(AD8) | !is.na(SBT)]$orlogid %>% uniqueN
+
+processed_preop_screen[, .(orlogid, AD8=`AD8 Dementia Score`, SBT=`Short Blessed Total Score` ) ] [ !is.na(AD8) ][!is.na(SBT)]$orlogid %>% uniqueN
+
+## these don't do anything
+processed_preop_screen[, .(orlogid, AD8=`AD8 Dementia Score`, SBT=`Short Blessed Total Score` ) ] %>% 
+merge(admit_outcomes[ , .(orlogid,ICU,  postop_los, readmit=fcase(is.na(readmission_survival) , FALSE, readmission_survival>30, FALSE, default=TRUE ) ) ] , by="orlogid") %>% pull("orlogid") %>% uniqueN
+
+processed_preop_screen[, .(orlogid, AD8=`AD8 Dementia Score`, SBT=`Short Blessed Total Score` ) ] %>% 
+merge(admit_outcomes[ , .(orlogid,ICU,  postop_los, readmit=fcase(is.na(readmission_survival) , FALSE, readmission_survival>30, FALSE, default=TRUE ) ) ] , by="orlogid") %>% merge(preop_covariates[, .(orlogid, death_date = `Patient Death Date` , RACE , Sex , age, COPD , CAD , CKD , CHF , CVA_Stroke , cancerStatus, Diabetes ) ] , by="orlogid" ) %>% pull("orlogid") %>% uniqueN
+
+## 33k age >= 65
+processed_preop_screen[, .(orlogid, AD8=`AD8 Dementia Score`, SBT=`Short Blessed Total Score` ) ] %>% 
+merge(admit_outcomes[ , .(orlogid,ICU,  postop_los, readmit=fcase(is.na(readmission_survival) , FALSE, readmission_survival>30, FALSE, default=TRUE ) ) ] , by="orlogid") %>% merge(preop_covariates[age >= 65, .(orlogid, death_date = `Patient Death Date` , RACE , Sex , age, COPD , CAD , CKD , CHF , CVA_Stroke , cancerStatus, Diabetes ) ] , by="orlogid" ) %>% pull("orlogid") %>% uniqueN
+
+## only 16 k have an AD8+SBT, 30k one or the other
+processed_preop_screen[, .(orlogid, AD8=`AD8 Dementia Score`, SBT=`Short Blessed Total Score` ) ][ !is.na(AD8) ][!is.na(SBT)] %>% 
+merge(admit_outcomes[ , .(orlogid,ICU,  postop_los, readmit=fcase(is.na(readmission_survival) , FALSE, readmission_survival>30, FALSE, default=TRUE ) ) ] , by="orlogid") %>% merge(preop_covariates[age >= 65, .(orlogid, death_date = `Patient Death Date` , RACE , Sex , age, COPD , CAD , CKD , CHF , CVA_Stroke , cancerStatus, Diabetes ) ] , by="orlogid" ) %>% pull("orlogid") %>% uniqueN
+
+processed_preop_screen[, .(orlogid, AD8=`AD8 Dementia Score`, SBT=`Short Blessed Total Score` ) ][ !is.na(AD8) | !is.na(SBT)] %>% 
+merge(admit_outcomes[ , .(orlogid,ICU,  postop_los, readmit=fcase(is.na(readmission_survival) , FALSE, readmission_survival>30, FALSE, default=TRUE ) ) ] , by="orlogid") %>% merge(preop_covariates[age >= 65, .(orlogid, death_date = `Patient Death Date` , RACE , Sex , age, COPD , CAD , CKD , CHF , CVA_Stroke , cancerStatus, Diabetes ) ] , by="orlogid" ) %>% pull("orlogid") %>% uniqueN
+
+## only 13k matching procedures, some procedures like avf are just uncommon in this data
+## supported by manually scanning for text
+
+## 40k procedure code data present
+procedure_codes$CSN %>% uniqueN
+
+## 15k age > 65
+procedure_codes[preop_covariates[age >= 65], on="orlogid"]$CSN %>% uniqueN
+
+## 4.8k included
+procedure_codes[preop_covariates[age >= 65], on="orlogid"][included==TRUE, CSN] %>% uniqueN
+
+procedure_codes[ , .(  gut_codes, stomach_codes, chole_codes, panc_codes, hyster_codes, lumbar_codes,shoulder_codes, hiatalHernia_codes, knee_codes, totalHip_codes, neph_codes,   prost_codes, bladder_codes, ueavfist_codes, vats_codes)] %>% sapply(sum)
+#          gut_codes      stomach_codes        chole_codes         panc_codes 
+#               3764               1213                609                556 
+#       hyster_codes       lumbar_codes     shoulder_codes hiatalHernia_codes 
+#               2343               1332               1205                183 
+#         knee_codes     totalHip_codes         neph_codes        prost_codes 
+#               1393               1432                692                823 
+#      bladder_codes     ueavfist_codes         vats_codes 
+#                206                267                162 
+
+## with ad8+sbt 3716
+
+processed_preop_screen[, .(orlogid, AD8=`AD8 Dementia Score`, SBT=`Short Blessed Total Score` ) ][ !is.na(AD8) ][!is.na(SBT)] %>% 
+merge(admit_outcomes[ , .(orlogid,ICU,  postop_los, readmit=fcase(is.na(readmission_survival) , FALSE, readmission_survival>30, FALSE, default=TRUE ) ) ] , by="orlogid") %>% merge(preop_covariates[age >= 65, .(orlogid, death_date = `Patient Death Date` , RACE , Sex , age, COPD , CAD , CKD , CHF , CVA_Stroke , cancerStatus, Diabetes ) ] , by="orlogid" ) %>% merge(procedure_codes[included==TRUE, .(orlogid, CSN, gut_codes, stomach_codes, chole_codes, panc_codes, hyster_codes, lumbar_codes,shoulder_codes, hiatalHernia_codes, knee_codes, totalHip_codes, neph_codes,   prost_codes, bladder_codes, ueavfist_codes, vats_codes, dispo) ] , by="orlogid") %>% pull("orlogid") %>% uniqueN
+
+}
+
+
 procedure_codes <- procedure_codes[included==TRUE]
 
-
-
-merged_data2 <- merged_data %>% merge(procedure_codes[, .(orlogid, CSN, gut_codes, stomach_codes, chole_codes, panc_codes, hyster_codes, lumbar_codes,shoulder_codes, hiatalHernia_codes, knee_codes, totalHip_codes, neph_codes,   prost_codes, bladder_codes, ueavfist_codes, vats_codes) ] , by="orlogid")
+merged_data2 <- merged_data %>% merge(procedure_codes[, .(orlogid, CSN, gut_codes, stomach_codes, chole_codes, panc_codes, hyster_codes, lumbar_codes,shoulder_codes, hiatalHernia_codes, knee_codes, totalHip_codes, neph_codes,   prost_codes, bladder_codes, ueavfist_codes, vats_codes, dispo) ] , by="orlogid")
 
 # merged_data2 <- merged_data %>% merge(dispo_holder2, by.x="orlogid", by.y="LOG_ID")
 
@@ -220,20 +313,23 @@ merged_data2[ , dc_home := dispo_type==3]
 }
 
 ## fixing up the death events as promised above
+merged_data2[, CSN:=as.character(CSN)]
+my_visits[, CSN:=as.character(CSN)]
+merged_data2 <- merge(merged_data2, my_visits, by="CSN", all.x=TRUE, all.y=FALSE)
 merged_data2[ , death := fcase(is.na(death_date), FALSE,  death_date < dist + ddays(30), TRUE, default=FALSE) ]
 
 merged_data2[ , .(  gut_codes, stomach_codes, chole_codes, panc_codes, hyster_codes, lumbar_codes,shoulder_codes, hiatalHernia_codes, knee_codes, totalHip_codes, neph_codes,   prost_codes, bladder_codes, ueavfist_codes, vats_codes)] %>% sapply(sum)
-# 
 #          gut_codes      stomach_codes        chole_codes         panc_codes 
-#                473                114                 38                135 
+#                677                161                 93                176 
 #       hyster_codes       lumbar_codes     shoulder_codes hiatalHernia_codes 
-#                161                377                365                  8 
+#                447                422                522                 50 
 #         knee_codes     totalHip_codes         neph_codes        prost_codes 
-#                244                360                 71                 67 
+#                504                427                140                282 
 #      bladder_codes     ueavfist_codes         vats_codes 
-#                 52                  8                 41 
+#                 87                 26                 50 
 
-## TODO: I don't know why some groups are so much smaller than expected. Maybe has to do with how the procedure set was queried. It may be set up to get inpatient only.
+
+## TODO: I don't know why some groups are so much smaller than expected. Maybe has to do with how the procedure set was queried. Maybe AV fistulas e.g. aren't going to preop
 
 pretty_names <- c("intestinal", "gastric", "cholecystectomy", "pancreatic", "hysterectomy", "lumbar fusion", "total shoulder", "lap hiatal hernia", "total knee", "total hip", "nephrectomy", "prostatectomy", "cystectomy", "AV fistula", "VATS" )
 
@@ -244,13 +340,13 @@ comborbid_vars <- c("COPD" , "CAD" , "CKD" , "CHF" , "CVA_Stroke" , "cancerStatu
 
 ## surgery specific effects - build formulas externally because of the non-factor structure
 ## save these building blocks for various models
-base_form <- "thisout ~ 0" %>% formula
+base_form <- "thisout ~ 1" %>% formula
 surg_vars <- colnames(merged_data2) %>% grep(pattern="_codes", value=T)
 surg_form <- paste0(surg_vars, collapse=" + ")
 surg_interact_form <- paste0(surg_vars,":AbnCog" ,  collapse=" + ")
 comorbid_form <- paste0(comborbid_vars ,  collapse=" + ")
 
-
+## TODO return here: no-intercept model isn't working even with adding sum(codes) [no-intercept implies reference = everyone else]
 
 myform <- base_form %>% 
   update( paste0("~.+", surg_form) ) %>%
@@ -259,6 +355,16 @@ myform <- base_form %>%
 
 library(dplyr)  
 library(splines)
+## surgery effects
+
+analysis_pipe <- . %>% mutate(thisout=dc_home) %>% mutate(AbnCog= as.numeric(AbnCog)) %>% 
+  glm(data=., formula=myform,  family=binomial() ) %>% 
+  summary %>% extract2("coefficients") %>% as_tibble(rownames="rname") %>% filter(grepl(rname, pattern="_codes")) %>% select(-`z value`)
+
+merged_data2 %>% analysis_pipe
+
+
+
 analysis_pipe <- . %>% mutate(thisout=dc_home) %>% mutate(AbnCog= as.numeric(AbnCog)) %>% 
   glm(data=., formula=myform,  family=binomial() ) %>% 
   summary %>% extract2("coefficients") %>% as_tibble(rownames="rname") %>% filter(grepl(rname, pattern="AbnCog")) %>% select(-`z value`)
@@ -271,6 +377,4 @@ myform <- base_form %>%
   update( paste0("~.+", comorbid_form) ) %>%
   update( "~.+bs(age, 5)" ) 
 merged_data2 %>% analysis_pipe
-
-
 
